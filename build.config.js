102da4425f1f9eb862ad6e32e1f7ceb00c0cdf76
abbodev <abbodev@gmail.com>
2026-06-16 23:16:51 +0200

    feat: change build system

--- build.config.js
+++
@@ -1,7 +0,0 @@
-export default {
-  builds: {
-    full: ["item", "baseitem", "spell", "bestiary"],
-    itemsOnly: ["item"],
-    bestiaryOnly: ["bestiary"],
-  },
-};
--- build.js
+++ build.js
@@ -1,16 +1,27 @@
 import fs from "fs-extra";
-import glob from "glob";
-import path from "path";
+import * as glob from "glob";
+import {Command} from "commander";
+
+const program = new Command();
+
+program
+  .version('0.0.1', '-v, --version')
+  .usage('[OPTIONS]...')
+  .option('-o, --output <value>', 'Output file', 'homebrew')
+  .parse(process.argv);
+
+const args = program.opts();
 
 const SRC_DIR = "./src";
-const OUTPUT = "./builds/homebrew.json";
+const OUTPUT_DIR = "./builds";
+const OUTPUT_FILE = `${OUTPUT_DIR}/${args.output}.json`;
 
 const CATEGORIES = ["item", "baseitem", "spell", "bestiary"];
 
 async function build() {
   const output = {
     _meta: {
-      sources: await fs.readJson("./src/_meta/sources.json"),
+      sources: await fs.readJson(`${SRC_DIR}/_meta/sources.json`),
     },
   };
 
@@ -21,7 +32,8 @@ async function build() {
 
     for (const file of files) {
       const data = await fs.readJson(file);
-      merged.push(...data);
+
+      merged.push(...Array.isArray(data) ? data : [data]);
     }
 
     if (merged.length) {
@@ -30,8 +42,8 @@ async function build() {
     }
   }
 
-  await fs.outputJson(OUTPUT, output, { spaces: 2 });
-  console.log("Build completata.");
+  await fs.outputJson(OUTPUT_FILE, output, { spaces: 2 });
+  console.log(`✅ Build completed! Output: ${OUTPUT_FILE}`);
 }
 
 build();
